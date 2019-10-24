78e8f7d0af69477fd9a1d2c6009663cc7cc08778
markwhittyunsw <z3099851@unsw.edu.au>
2019-10-24 11:43:30 +1100

    removed conio from arraypointers and added data file

--- ArrayPointers2D.cpp
+++ ArrayPointers2D.cpp
@@ -1,7 +1,6 @@
 // Author: Jay Katiputiya and Mark Whitty
 #include <iostream>
 #include <iomanip>
-#include <conio.h>
 
 using namespace std;
 
@@ -11,17 +10,18 @@ int main()
 	double (*DblRow)[2];
 
 	cout << "Address of M in hex is \t\t\t" << hex << M << endl;
+	cout << endl;
 
 	cout << "Value of *M in hex is \t\t\t" << hex << *M  << endl;
 	cout << "Value of *M+1 in hex is \t\t" << hex << *M+1  << endl;
 	cout << "Value of *(*M+1) in dec is \t\t" << dec << *(*M+1)  << endl;
+	cout << endl;
 
 	DblRow = M;
 
 	cout << "Value of DblRow pointer in hex is \t" << hex << DblRow  << endl;
 	cout << "Value of DblRow+1 in hex is  \t\t" << hex << (DblRow+1) << endl;
+	cout << endl;
 
-
-	_getch();
 	return 0;
 }
